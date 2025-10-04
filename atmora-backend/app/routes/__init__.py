5049c6b88cf45229c04028574e12455b7532f7f1
Hakan BAĞIÇ <hbagic@makersan.com>
2025-10-04 10:34:03 +0300

    Initial commit

---
+++ atmora-backend/app/routes/__init__.py
@@ -0,0 +1,8 @@
+'''from .hello import hello_bp
+from .users import users_bp
+from .products import products_bp
+
+def register_routes(app):
+    app.register_blueprint(hello_bp, url_prefix='/api')
+    app.register_blueprint(users_bp, url_prefix='/api/users')
+    app.register_blueprint(products_bp, url_prefix='/api/products')'''
