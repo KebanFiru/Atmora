9eb7d80eebc802453b6b32108dd6d19e915bbb9c
Hakan BAĞIÇ <hbagic@makersan.com>
2025-10-04 12:34:13 +0300

    Added full map support

--- atmora_frontend/app/layout.tsx
+++ atmora_frontend/app/layout.tsx
@@ -1,17 +1,19 @@
-import 'leaflet/dist/leaflet.css'; // ✅ Leaflet CSS
-import './globals.css';            // ✅ Tailwind CSS
+import './globals.css';
+import 'leaflet/dist/leaflet.css';
+import Main from './main/page';
 
-import type { Metadata } from 'next';
+export const metadata = {
+  title: "Atmora",
+  description: "Atmora Weather App",
+}
 
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Main/>
+        {children}
+      </body>
     </html>
   );
 }
